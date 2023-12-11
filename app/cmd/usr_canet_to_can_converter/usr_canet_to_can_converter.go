177f505d36c60c24497157b2998fd01436eead38
Diego Maldonado <dmnunez1993@gmail.com>
2023-12-11 19:38:25 -0300

    added rest api for admin

--- app/cmd/usr_canet_to_can_converter/usr_canet_to_can_converter.go
+++ app/cmd/usr_canet_to_can_converter/usr_canet_to_can_converter.go
@@ -3,7 +3,10 @@ package main
 import usrcanettocan "github.com/dmnunez1993/usr-canet200-to-can-converter"
 
 func main() {
-	_ = usrcanettocan.NewUsrCanetConverter()
+	converter := usrcanettocan.NewUsrCanetConverter()
+	go converter.Run()
+
+	go usrcanettocan.ServeRestApi()
 
 	usrcanettocan.LoopForever()
 }
