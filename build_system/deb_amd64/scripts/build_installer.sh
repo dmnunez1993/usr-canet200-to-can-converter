7423bf810b397fbe469b32b8a979b802c5a22c70
Diego Maldonado <dmnunez1993@gmail.com>
2023-12-13 16:44:54 -0300

    fix errors with systemctl

--- build_system/deb_amd64/scripts/build_installer.sh
+++ build_system/deb_amd64/scripts/build_installer.sh
@@ -1,20 +1,23 @@
 #!/usr/bin/env bash
 
+BUILD_FOLDER=/tmp/usr_canet200_to_can/build
 OUTPUT_FOLDER=/output
+
+mkdir -p $BUILD_FOLDER
 
 cd /usr-canet200-to-can-converter/app
 
 echo "Cleaning up previous layout..."
-rm -rf $OUTPUT_FOLDER/src
-rm -rf $OUTPUT_FOLDER/admin
+rm -rf $BUILD_FOLDER/src
+rm -rf $BUILD_FOLDER/admin
 
-mkdir -p $OUTPUT_FOLDER
-mkdir -p $OUTPUT_FOLDER/src/usr_canet200_to_can_converter/usr/local/usr_canet200_to_can_converter
+mkdir -p $BUILD_FOLDER
+mkdir -p $BUILD_FOLDER/src/usr_canet200_to_can_converter/usr/local/usr_canet200_to_can_converter
 
-DEBIAN_FOLDER=$OUTPUT_FOLDER/src/usr_canet200_to_can_converter/DEBIAN
-ROOT_FOLDER=$OUTPUT_FOLDER/src/usr_canet200_to_can_converter
-SRC_FOLDER=$OUTPUT_FOLDER/src/usr_canet200_to_can_converter/usr/local
-VAR_FOLDER=$OUTPUT_FOLDER/src/usr_canet200_to_can_converter/var/usr_canet200_to_can_converter
+DEBIAN_FOLDER=$BUILD_FOLDER/src/usr_canet200_to_can_converter/DEBIAN
+ROOT_FOLDER=$BUILD_FOLDER/src/usr_canet200_to_can_converter
+SRC_FOLDER=$BUILD_FOLDER/src/usr_canet200_to_can_converter/usr/local
+VAR_FOLDER=$BUILD_FOLDER/src/usr_canet200_to_can_converter/var/usr_canet200_to_can_converter
 REPOSITORY_ROOT=/usr-canet200-to-can-converter
 
 mkdir -p $VAR_FOLDER
@@ -32,8 +35,8 @@ env GOOS=linux GOARCH=amd64 CGO_ENABLED=1\
 # Build NodeJS admin
 cd /usr-canet200-to-can-converter
 # Exclude files that might be present if developing on the same machine
-rsync -av --progress admin $OUTPUT_FOLDER --exclude admin/node_modules/ --exclude admin/build/
-cd $OUTPUT_FOLDER/admin
+rsync -av --progress admin $BUILD_FOLDER --exclude admin/node_modules/ --exclude admin/build/
+cd $BUILD_FOLDER/admin
 yarn install
 yarn build
 mkdir -p $SRC_FOLDER/usr_canet200_to_can_converter/admin
@@ -45,6 +48,11 @@ cp -r $REPOSITORY_ROOT/build_system/deb_amd64/metadata/DEBIAN $DEBIAN_FOLDER
 echo "Setting up remaining files..."
 cp -a $REPOSITORY_ROOT/build_system/deb_amd64/additional/. $ROOT_FOLDER
 
+# Create the deb installer
 echo "Creating debian package..."
-cd $OUTPUT_FOLDER/src/
-fakeroot dpkg-deb --build usr_canet200_to_can_converter $OUTPUT_FOLDER/usr_canet200_to_can_converter.deb
+cd $BUILD_FOLDER/src/
+fakeroot dpkg-deb --build usr_canet200_to_can_converter $BUILD_FOLDER/usr_canet200_to_can_converter.deb
+
+# Copy the installer to the output folder and cleanup
+cp $BUILD_FOLDER/usr_canet200_to_can_converter.deb $OUTPUT_FOLDER
+rm -rf $BUILD_FOLDER
